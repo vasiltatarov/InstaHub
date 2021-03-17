6ff2751af3346e3da99005499693b2e14752801b
vasiltatarov <vasiltatarov3@gmail.com>
2021-03-17 18:29:11 +0200

    Implement profile page with all users

---
+++ src/Web/MyForum.Web.ViewModels/Profiles/ProfileViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace MyForum.Web.ViewModels.Profiles
+{
+
+    using AutoMapper;
+    using MyForum.Data.Models;
+    using MyForum.Services.Mapping;
+
+    public class ProfileViewModel : IMapFrom<ApplicationUser>, IHaveCustomMappings
+    {
+        public string Username { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public int PostsCount { get; set; }
+
+        public int PostViews { get; set; }
+
+        public int VotesCount { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration
+                .CreateMap<ApplicationUser, ProfileViewModel>()
+                .ForMember(x => x.Username,
+                    y => y.MapFrom(x => x.UserName))
+                .ForMember(x => x.PostViews,
+                    y => y.MapFrom(x => x.Posts.Sum(p => p.VisitorsCount)));
+        }
+    }
+}

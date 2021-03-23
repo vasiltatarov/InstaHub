009df2e4a7ae259817dbb03977f7244e353dffae
vasiltatarov <vasiltatarov3@gmail.com>
2021-03-24 01:37:32 +0200

    implement pagination in saved posts page

--- src/Web/MyForum.Web/Controllers/UserSavedPostsController.cs
+++ src/Web/MyForum.Web/Controllers/UserSavedPostsController.cs
@@ -8,10 +8,13 @@
     using MyForum.Data.Models;
     using MyForum.Services.Data;
     using MyForum.Web.ViewModels.UserSavedPosts;
+    using PagedList;
 
     [Authorize]
     public class UserSavedPostsController : Controller
     {
+        private const int ItemsOnPaged = 5;
+
         private readonly UserManager<ApplicationUser> userManager;
         private readonly IUserSavedPostsService userSavedPostsService;
 
@@ -31,10 +34,12 @@
         }
 
         [Authorize]
-        public IActionResult GetSavedPosts()
+        public IActionResult GetSavedPosts(int page = 1)
         {
             var userId = this.userManager.GetUserId(this.User);
-            var posts = this.userSavedPostsService.GetUserSavedPosts<SavePostViewModel>(userId);
+            var posts = this.userSavedPostsService.
+                GetUserSavedPosts<SavePostViewModel>(userId)
+                .ToPagedList(page, ItemsOnPaged);
 
             return this.View(posts);
         }

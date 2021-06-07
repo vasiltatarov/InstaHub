575a4291dc3fb3330c43b3b15536b693591968ce
vasiltatarov <vasiltatarov3@gmail.com>
2021-06-08 00:31:23 +0300

    remove unnecessary files

--- src/MyForum.Common/GlobalConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyForum.Common
-{
-    public static class GlobalConstants
-    {
-        public const string SystemName = "MyForum";
-
-        public const string AdministratorRoleName = "Administrator";
-    }
-}

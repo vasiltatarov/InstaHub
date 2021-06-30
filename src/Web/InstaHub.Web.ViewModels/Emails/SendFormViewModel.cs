91f6a3f969aa3841ed3f313dcf604bd3edecb38f
vasiltatarov <vasiltatarov3@gmail.com>
2021-06-30 14:27:57 +0300

    implement send form email view model

---
+++ src/Web/InstaHub.Web.ViewModels/Emails/SendFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace InstaHub.Web.ViewModels.Emails
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class SendFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string To { get; set; }
+    }
+}
--- src/Web/InstaHub.Web/Controllers/EmailsController.cs
+++ src/Web/InstaHub.Web/Controllers/EmailsController.cs
@@ -5,11 +5,13 @@
     using InstaHub.Data.Models;
     using InstaHub.Services.Data;
     using InstaHub.Services.Messaging;
+    using InstaHub.Web.ViewModels.Emails;
     using InstaHub.Web.ViewModels.Posts;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
 
+    [Authorize]
     public class EmailsController : Controller
     {
         private readonly IPostsService postsService;
@@ -23,17 +25,23 @@
             this.userManager = userManager;
         }
 
-        [Authorize]
+        public IActionResult SendPostToEmail() => this.View();
+
         [HttpPost]
-        public async Task<IActionResult> SendPostToEmail(int id)
+        public async Task<IActionResult> SendPostToEmail(SendFormViewModel form)
         {
-            var post = await this.postsService.GetById<PostViewModel>(id);
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(form);
+            }
+
+            var post = await this.postsService.GetById<PostViewModel>(form.Id);
             var user = await this.userManager.GetUserAsync(this.User);
 
             await this.emailSender
-                .SendEmailAsync("vasiltatarov3@gmail.com", "Vasko", "vasiltatarov3@gmail.com", post.Title, post.Content);
+                .SendEmailAsync(user.Email, user.UserName, form.To, post.Title, post.Content);
 
-            return this.RedirectToAction("ById", "Posts", new { id });
+            return this.RedirectToAction("ById", "Posts", new { form.Id });
         }
     }
 }

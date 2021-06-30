e3a18f38e3d82894df272039eb989748ed99eefb
vasiltatarov <vasiltatarov3@gmail.com>
2021-06-30 18:12:41 +0300

    refactor profile view model

--- src/Web/InstaHub.Web.ViewModels/Profiles/ProfileViewModel.cs
+++ src/Web/InstaHub.Web.ViewModels/Profiles/ProfileViewModel.cs
@@ -9,11 +9,9 @@
 
     public class ProfileViewModel : IMapFrom<ApplicationUser>, IHaveCustomMappings
     {
-        public string Username { get; set; }
+        public string UserName { get; set; }
 
         public string Location { get; set; }
-
-        public string ImagePath { get; set; }
 
         public string Description { get; set; }
 
@@ -21,18 +19,16 @@
 
         public int PostViews { get; set; }
 
-        public int VotesCount { get; set; }
-
         public UserProfilePostViewModel[] Posts { get; set; }
 
         public void CreateMappings(IProfileExpression configuration)
         {
             configuration
                 .CreateMap<ApplicationUser, ProfileViewModel>()
-                .ForMember(x => x.Username,
-                    y => y.MapFrom(x => x.UserName))
-                .ForMember(x => x.PostViews,
-                    y => y.MapFrom(x => x.Posts.Sum(p => p.VisitorsCount)));
+                .ForMember(
+                    x => x.PostViews,
+                    y => y
+                        .MapFrom(x => x.Posts.Sum(p => p.VisitorsCount)));
         }
     }
 }

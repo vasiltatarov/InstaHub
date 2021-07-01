f991d65c6059ad0e1fb52be12656bc3d6e3d3198
vasiltatarov <vasiltatarov3@gmail.com>
2021-07-01 11:24:02 +0300

    implement contact form

---
+++ src/Web/InstaHub.Web.ViewModels/Contacts/ContactFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace InstaHub.Web.ViewModels.Contacts
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ContactFormViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
---
+++ src/Web/InstaHub.Web/Controllers/ContactsController.cs
@@ -0,0 +1,32 @@
+﻿namespace InstaHub.Web.Controllers
+{
+    using InstaHub.Web.ViewModels.Contacts;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ContactsController : Controller
+    {
+        private const string RedirectedFromContactForm = "RedirectedFromContactForm";
+
+        public IActionResult Index() => this.View();
+
+        [HttpPost]
+        public IActionResult Index(ContactFormViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            //var ip = this.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            this.TempData[RedirectedFromContactForm] = true;
+
+            return this.RedirectToAction("ThankYou");
+        }
+
+        public IActionResult ThankYou()
+            => this.TempData[RedirectedFromContactForm] == null
+                ? this.NotFound()
+                : this.View();
+    }
+}
